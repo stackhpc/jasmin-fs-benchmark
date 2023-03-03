3e215f20fb2066c1f17326efd7d4f084bc503009
Steve Brasier <steveb@stackhpc.com>
2023-03-03 10:03:56 +0000

    add input files for full matrix of 10-node pure runs

---
+++ io500/runs/pure-10-16-30-1-MPIIO/io500.pure-10-16-30-1-MPIIO.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=io500
+#SBATCH --account=stackhpc
+#SBATCH --partition=stackhpc
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=16
+#SBATCH --dependency=singleton
+#SBATCH --time=2:0:0
+
+set -euo pipefail
+
+export IO500_CONTAINER_TAG="bb942db"
+
+echo git describe: heads/main-0-gcbf28c2-dirty
+echo SLURM_JOB_ID: $SLURM_JOB_ID
+echo dimensions: '{"nodes": 10, "ntasks_per_node": 16, "filesystem": {"name": "pure", "mountpoint": "/work/stackhpc-pure/"}, "stonewall": 30, "iters": 1, "time": "2:0:0", "git_describe": "heads/main-0-gcbf28c2-dirty", "ior_api": "MPIIO"}'
+echo SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST
+
+mkdir -p /work/stackhpc-pure/steveb
+module load eb/OpenMPI/gcc/4.0.0
+timestamp=$(date +%Y.%m.%d-%H.%M.%S)
+mpirun singularity exec --bind /work/stackhpc-pure/:/work/stackhpc-pure/ docker://ghcr.io/stackhpc/io500-singularity:${IO500_CONTAINER_TAG} /io500 /home/users/steveb/io500-lotus/io500/runs/pure-10-16-30-1-MPIIO/config.pure-10-16-30-1-MPIIO.ini --timestamp $timestamp
