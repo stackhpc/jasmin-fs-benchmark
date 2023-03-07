2ac24c714ab19a8c9aae5962e0f25eb2218592a2
Steve Brasier <steveb@stackhpc.com>
2023-03-07 20:29:18 +0000

    start adding pansas results

---
+++ io500/runs/pansas-1-16-30-1-POSIX/io500.pansas-1-16-30-1-POSIX.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=io500
+#SBATCH --account=stackhpc
+#SBATCH --partition=stackhpc
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --dependency=singleton
+#SBATCH --time=2:0:0
+
+set -euo pipefail
+
+export IO500_CONTAINER_TAG="bb942db"
+
+echo git describe: heads/main-0-gfd48d0d-dirty
+echo SLURM_JOB_ID: $SLURM_JOB_ID
+echo dimensions: '{"nodes": 1, "ntasks_per_node": 16, "filesystem": {"name": "pansas", "mountpoint": "/gws/pw/j07/perf_testing/stackhpc"}, "stonewall": 30, "iters": 1, "time": "2:0:0", "git_describe": "heads/main-0-gfd48d0d-dirty", "ior_api": "POSIX"}'
+echo SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST
+
+mkdir -p /gws/pw/j07/perf_testing/stackhpc/steveb
+module load eb/OpenMPI/gcc/4.0.0
+timestamp=$(date +%Y.%m.%d-%H.%M.%S)
+mpirun singularity exec --bind /gws/pw/j07/perf_testing/stackhpc:/gws/pw/j07/perf_testing/stackhpc docker://ghcr.io/stackhpc/io500-singularity:${IO500_CONTAINER_TAG} /io500 /home/users/steveb/io500-lotus/io500/runs/pansas-1-16-30-1-POSIX/config.pansas-1-16-30-1-POSIX.ini --timestamp $timestamp
---
+++ io500/runs/pansas-2-2-300-0-POSIX/io500.pansas-2-2-300-0-POSIX.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=io500
+#SBATCH --account=stackhpc
+#SBATCH --partition=stackhpc
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=2
+#SBATCH --dependency=singleton
+#SBATCH --time=2:0:0
+
+set -euo pipefail
+
+export IO500_CONTAINER_TAG="bb942db"
+
+echo git describe: heads/main-0-gfd48d0d-dirty
+echo SLURM_JOB_ID: $SLURM_JOB_ID
+echo dimensions: '{"nodes": 2, "ntasks_per_node": 2, "filesystem": {"name": "pansas", "mountpoint": "/gws/pw/j07/perf_testing/stackhpc"}, "stonewall": 300, "iters": 0, "time": "2:0:0", "git_describe": "heads/main-0-gfd48d0d-dirty", "ior_api": "POSIX"}'
+echo SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST
+
+mkdir -p /gws/pw/j07/perf_testing/stackhpc/steveb
+module load eb/OpenMPI/gcc/4.0.0
+timestamp=$(date +%Y.%m.%d-%H.%M.%S)
+mpirun singularity exec --bind /gws/pw/j07/perf_testing/stackhpc:/gws/pw/j07/perf_testing/stackhpc docker://ghcr.io/stackhpc/io500-singularity:${IO500_CONTAINER_TAG} /io500 /home/users/steveb/io500-lotus/io500/runs/pansas-2-2-300-0-POSIX/config.pansas-2-2-300-0-POSIX.ini --timestamp $timestamp
